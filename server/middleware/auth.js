85da1d596c66f62b74949047f62de3733b98a1aa
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 02:51:49 +0530

    fix: Rectified Critical Errors

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken')
 
-const User = require('../models/User ')
+const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
 
 exports.protect = async(req, res, next) => {
     let token
 
-    if (req.headers.authorization && req.headers.startsWith("Bearer")){
-        token = req.headers.authorization.split('')[1]
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        token = req.headers.authorization.split(' ')[1]
     }
 
     if(!token){
