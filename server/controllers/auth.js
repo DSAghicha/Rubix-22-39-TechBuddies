5c50b036bffe5506adb9d402264244a250d84717
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 01:27:48 +0530

    Added Login Logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,8 +20,45 @@ exports.register = async (req, res, next) => {
     }
 }
 
-exports.login = (req, res, next) => {
-    res.send("Login Route")
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body
+    
+    if(!email || !password) {
+        res.status(400).json({
+            success: false,
+            error: "Please provide email and password"
+        })
+    }
+
+    try {
+        const user = await User.findOne({ email }).select("+password")
+
+        if(!user){
+            res.status(404).json({
+                success: false,
+                error: "User is not registered!"
+            })
+        }
+
+        const isMatch = await user.matchPasswords(password)
+
+        if(!isMatch){
+            res.status(404).json({
+                success: false,
+                error: "Password is incorrect."
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "tr1452asdf"
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            error: error.message
+        })
+    }
 }
 
 exports.forgotpassword = (req, res, next) => {
