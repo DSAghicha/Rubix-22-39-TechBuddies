472dd658f5f0f05ba440de98a31e992ffb980084
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 01:51:26 +0530

    Implementing Error Handler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const ErrorResponse = require('../utils/errorResponse')
 
 exports.register = async (req, res, next) => {
     const {first_name, last_name='', email, password} = req.body
@@ -13,10 +14,7 @@ exports.register = async (req, res, next) => {
             user
         })
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message
-        })
+        next(error)
     }
 }
 
@@ -24,29 +22,20 @@ exports.login = async (req, res, next) => {
     const { email, password } = req.body
     
     if(!email || !password) {
-        res.status(400).json({
-            success: false,
-            error: "Please provide email and password"
-        })
+        return next(new ErrorResponse('Please provide email and password', 400))
     }
 
     try {
         const user = await User.findOne({ email }).select("+password")
 
         if(!user){
-            res.status(404).json({
-                success: false,
-                error: "User is not registered!"
-            })
+            return next(new ErrorResponse('User is not registered!', 401))
         }
 
         const isMatch = await user.matchPasswords(password)
 
         if(!isMatch){
-            res.status(404).json({
-                success: false,
-                error: "Password is incorrect."
-            })
+            return next(new ErrorResponse('Password is incorrect!', 401))
         }
 
         res.status(200).json({
@@ -54,10 +43,7 @@ exports.login = async (req, res, next) => {
             token: "tr1452asdf"
         })
     } catch (error) {
-        res.status(404).json({
-            success: false,
-            error: error.message
-        })
+        return next(new ErrorResponse(error.message, 500))
     }
 }
 
