ec629730d38c75887904d8357f98f005c8cf3e95
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 09:02:27 +0530

    Created Reset Password Login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require('../utils/sendEmail')
 
 exports.register = async (req, res, next) => {
     const {first_name, last_name='', email, password} = req.body
@@ -41,8 +42,48 @@ exports.login = async (req, res, next) => {
     }
 }
 
-exports.forgotpassword = (req, res, next) => {
-    res.send("Forgot Password Route")
+exports.forgotpassword = async(req, res, next) => {
+    const { email } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+
+        if(!user){
+            return next(new ErrorResponse('Email could not ne sent', 404))
+        }
+        const resetToken = user.getResetPasswordToken()
+
+        await user.save()
+
+        const resetURL = `http://localhost:3000/passwordreset/${ resetToken }`
+        const message =
+        `<h1> It seems like you have forgotten your password.</h1>
+        <p>
+            <a href=${resetURL} clicktracking=off>Click here</a>
+            to reset your password.`
+
+        try {
+            await sendEmail({
+                to: user.email,
+                subject: "Password Reset",
+                text: message
+            })
+            res.status(200).json({
+                success:true,
+                data: "Email Sent"
+            })
+        } catch (error) {
+            user.getResetPasswordToken = undefined
+            user.resetPasswordExpire = undefined
+
+            await user.save()
+
+            return next(new ErrorResponse("Email could not be sent.", 500))
+        }
+    } catch (error) {
+        next(error)
+        console.error(error);
+    }
 }
 
 exports.resetpassword = (req, res, next) => {
