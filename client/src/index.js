c0d1d9007dd753d22c7d2045f97f079bcd16a262
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 11:19:49 +0530

    Added Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { PrivateRoute } from './components'
+
+import { Dashboard, ForgotPassword, Home, Login, Register, ResetPassword } from './pages'
 
 ReactDOM.render(
-    <div>
-        <h1>Hello World!</h1>
-    </div>,
+    <Router>
+        <Routes>
+            <Route exact path='/' element={ <Home/> }/>
+            <Route exact path='/login' element={ <Login/> }/>
+            <Route exact path='/register' element={ <Register/> }/>
+            <Route exact path='/forgotpassword' element={ <ForgotPassword/> }/>
+            <Route exact path='/passwordreset/:resetToken' element={ <ResetPassword/> }/>
+
+            <Route exact path='/dashboard' element={<PrivateRoute/>}>
+                <Route exact path='/dashboard/:userId' element={<Dashboard/>}/>
+            </Route>
+        </Routes>
+    </Router>,
     document.getElementById('root')
 )
