a784df1bb27f1f82b6ef52484771224ffe86b1b7
DSAghicha <dsaghicha211@gmail.com>
2022-01-20 11:20:31 +0530

    Creating Pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './home/Home'
+export { default as Login } from './userAuth/Login'
+export { default as Register } from './userAuth/Register'
+export { default as ForgotPassword } from './userAuth/ForgotPassword'
+export { default as ResetPassword } from './userAuth/ResetPassword'
+
+export { default as Dashboard } from './dashboard/Dashboard'
